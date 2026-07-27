52b84cddf027f97d2269bdff8ef59ea377a1ec67
supun_yasantha034 <supunyasantha.official@gmail.com>
2026-07-27 10:38:40 +0530

    import and call database_service.dart.

--- lib/data/category_data.dart
+++ lib/data/category_data.dart
@@ -1,14 +1,10 @@
 import '../models/category_model.dart';
+import '../services/database_service.dart';
 
 class CategoryData {
-  static List<Category> getCategories() {
-    return [
-      Category(id: '1', name: 'Featured', icon: '⭐', itemCount: 4),
-      Category(id: '2', name: 'Beauty & Health', icon: '💄', itemCount: 3),
-      Category(id: '3', name: 'Clothing', icon: '👕', itemCount: 3),
-      Category(id: '4', name: 'Sports', icon: '⚽', itemCount: 3),
-      Category(id: '5', name: 'Accessories', icon: '🕶️', itemCount: 3),
-      Category(id: '6', name: 'Fashion', icon: '👠', itemCount: 3),
-    ];
+  static final DatabaseService _dbService = DatabaseService();
+
+  static Future<List<Category>> getCategories() async {
+    return await _dbService.getCategories();
   }
 }
