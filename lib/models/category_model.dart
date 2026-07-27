ef40d5a9a37f01687d1412a74aeb501fe0d8c6ec
supun_yasantha034 <supunyasantha.official@gmail.com>
2026-07-27 11:57:46 +0530

    implementing a Category.fromMap.

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -10,4 +10,17 @@ class Category {
     required this.icon,
     required this.itemCount,
   });
+
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'name': name, 'icon': icon, 'item_count': itemCount};
+  }
+
+  factory Category.fromMap(Map<String, dynamic> map) {
+    return Category(
+      id: map['id'] ?? '',
+      name: map['name'] ?? '',
+      icon: map['icon'] ?? '',
+      itemCount: map['item_count'] ?? 0,
+    );
+  }
 }
